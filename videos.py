aba7f4bcc68a590be6628e5c491c70be50296ea8
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-10 12:32:36 +0300

    Update videos.py

--- videos.py
+++ videos.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import streamlit as st
 from pathlib import Path
-from typing import Literal
 from streamlit_webrtc import (
     ClientSettings,
     VideoTransformerBase,
@@ -11,117 +10,70 @@ from streamlit_webrtc import (
     webrtc_streamer,
 )
 
-# Import your video editing functions
-from modules.Cartoon.main import cartoonify
-from modules.SmoothSlider.main import half_slide
-from modules.ThugLife.main import overlay_thuglife
-from modules.TimeFreeze.main import time_freeze
-from modules.ScanFreeze.main import line_freeze
-from modules.HeartEyes.main import overlay_heart_eyes
-from modules.Moustaches.main import overlay_moustache
-from modules.BlurFaces.main import blur_all_faces
-from modules.DevilFace.main import horns_nd_fangs_overlay
-from helper.descriptor import filter_info
-from helper.utils import file_checker
-
 HERE = Path(__file__).parent
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
     media_stream_constraints={"video": True, "audio": False},
 )
 
-def video_filters(transform_type):
-    """ Video transforms with OpenCV """
+class VideoRecorder:
+    def __init__(self):
+        self.frames = []
+        self.recording = False
 
-    file_checker_return = file_checker(transform_type)
+    def add_frame(self, frame):
+        if self.recording:
+            self.frames.append(frame)
+            if len(self.frames) > 10 * 30:  # Maximum recording length: 10 seconds
+                self.stop_recording()
 
-    class OpenCVVideoTransformer(VideoTransformerBase):
-        type: Literal[
-            "Live Stream",
-            "Cartoonie",
-            "Half Slide - Horizontal",
-            "Half Slide - Vertical",
-            "Line Freeze - Horizontal",
-            "Line Freeze - Vertical",
-            "Thug Life",
-            "Time Freeze - Ssim",
-            "Time Freeze - Rcnn",
-            "Heart Eyes",
-            "Moustaches",
-            "Face Blur",
-            "Devil-ie"
-        ]
+    def start_recording(self):
+        self.recording = True
+        self.frames = []
 
-        def __init__(self) -> None:
-            self.type = "Live Stream"
-            # Initialize any required variables for video editing
-            # ...
+    def stop_recording(self):
+        self.recording = False
 
-        def transform(self, frame: av.VideoFrame) -> av.VideoFrame:
-            img = frame.to_ndarray(format="bgr24")
-            img = cv2.flip(img, 1)
+    def get_recorded_video(self):
+        return self.frames
 
-            if self.type == "Live Stream":
-                pass
-            elif self.type == "Half Slide - Horizontal":
-                # Call your half_slide function
-                img = half_slide(img, self.last_frame, "horizontal")
-                self.last_frame = img
-            elif self.type == "Half Slide - Vertical":
-                # Call your half_slide function
-                img = half_slide(img, self.last_frame, "vertical")
-                self.last_frame = img
-            elif self.type == "Line Freeze - Horizontal":
-                # Call your line_freeze function
-                img = line_freeze(img, self.final_array, self.x, "horizontal")
-                self.final_array = img
-                self.x += 1
-            elif self.type == "Line Freeze - Vertical":
-                # Call your line_freeze function
-                img = line_freeze(img, self.final_array, self.y, "vertical")
-                self.final_array = img
+class OpenCVVideoTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.recorder = VideoRecorder()
 
-            # Rest of the code for other transformations...
+    def transform(self, frame: av.VideoFrame) -> av.VideoFrame:
+        img = frame.to_ndarray(format="bgr24")
+        img = cv2.flip(img, 1)
 
-            return img
+        self.recorder.add_frame(img)
 
-    filter_info(transform_type)
-
-    if file_checker_return[0] == True:
-        webrtc_ctx = webrtc_streamer(
-            key="opencv-filter",
-            mode=WebRtcMode.SENDRECV,
-            client_settings=WEBRTC_CLIENT_SETTINGS,
-            video_transformer_factory=OpenCVVideoTransformer,
-            async_transform=True,
-        )
-
-        if webrtc_ctx.video_transformer:
-            webrtc_ctx.video_transformer.type = transform_type
+        return img
 
 def main():
-    transform_types = [
-        "Live Stream",
-        "Cartoonie",
-        "Half Slide - Horizontal",
-        "Half Slide - Vertical",
-        "Line Freeze - Horizontal",
-        "Line Freeze - Vertical",
-        "Thug Life",
-        "Time Freeze - Ssim",
-        "Time Freeze - Rcnn",
-        "Heart Eyes",
-        "Moustaches",
-        "Face Blur",
-        "Devil-ie"
-    ]
-
     st.title("Video Editor")
 
-    selected_transform = st.selectbox("Video Transformations", transform_types)
-    st.text("Selected Transformation: " + selected_transform)
+    recorder = VideoRecorder()
 
-    video_filters(selected_transform)
+    webrtc_ctx = webrtc_streamer(
+        key="opencv-filter",
+        mode=WebRtcMode.SENDRECV,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
+        video_transformer_factory=OpenCVVideoTransformer,
+        async_transform=True,
+    )
+
+    if webrtc_ctx.state.playing:
+        if st.button("Start Recording"):
+            recorder.start_recording()
+
+        if st.button("Stop Recording"):
+            recorder.stop_recording()
+            recorded_video = recorder.get_recorded_video()
+
+            if recorded_video:
+                st.video(np.array(recorded_video))
+            else:
+                st.warning("No video recorded.")
 
 if __name__ == "__main__":
     main()
